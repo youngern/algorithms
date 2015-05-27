278cf6280c336cf2ae68fc1607c96c56bf5c3f6a
songyen <songye@cs.utexas.edu>
2015-05-27 00:14:56 -0500

    clean up and add test cases

--- spiral.c
+++ spiral.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 #include <stdint.h>
 
-/*given a 2-D array, print values in a out-in spiral*/
+/*
+   given a 2-D array with numbers arranged in such a way that
+   it forms an left to right, inward spiral, print the numbers in the
+   correct order.
+   i.e.
+   {{1, 8, 7},
+    {2, 9, 6},   =>  1 2 3 4 5 6 7 8 9
+    {3, 4, 5}} 
+
+   {{1, 10, 9, 8}
+    {2, 11, 12, 7}  => 1 2 3 4 5 6 7 8 9 10 11 12
+    {3, 4 ,5 ,6}}
+
+
+*/
 enum direction {DOWN, RIGHT, UP, LEFT};
 
 void printSpiral(int rows, int cols, int spiral[rows][cols], int size){
@@ -9,42 +23,40 @@ void printSpiral(int rows, int cols, int spiral[rows][cols], int size){
   int rightEnd = cols - 1;
   int downEnd =  rows - 1;
   int leftEnd = 0, upEnd = 0;
-  printf("%d\n", spiral[1][0]);
   enum direction currDir = DOWN;
   while(count < size){
     count++;
     switch(currDir){
       case DOWN:
-        printf("%d ",spiral[row][col]);
-        if(row == downEnd){
+        printf ("%d ",spiral[row][col]);
+        if (row == downEnd) {
             downEnd--;
             leftEnd++;
             currDir = RIGHT;
             col++;
-        }
-        else 
+        } else { 
             row++;
-        
+        }
         break;
       case RIGHT:
-        printf("%d ",spiral[row][col]);       
-        if(col == rightEnd){
+        printf ("%d ",spiral[row][col]);       
+        if (col == rightEnd) {
             rightEnd--;
             currDir = UP;
             row--;
-        }
-        else
+        } else {
             col++;
+        }
         break;
       case UP:
-        printf("%d ", spiral[row][col]); 
-        if(row == upEnd){
+        printf ("%d ", spiral[row][col]); 
+        if (row == upEnd) {
             upEnd ++;
             currDir = LEFT;
             col--;
-        }
-        else
+        } else {
             row--;
+        }
         break;
       case LEFT:
         printf("%d ",spiral[row][col]);
@@ -52,20 +64,38 @@ void printSpiral(int rows, int cols, int spiral[rows][cols], int size){
             leftEnd++;
             currDir = DOWN;
             row++;
-        }
-        else
+        } else {
             col--;
+        }
     }
+  }
+  printf("\n\n");
+}
+
+void printArray(int rows, int cols, int array[rows][cols]) {
+  for(int i = 0; i < rows; i++) {
+    for(int j = 0; j < cols; j++){
+       printf("%3d ", array[i][j]);
+    }
+    printf("\n");
   }
 
 }
-
-int main(){   
-   int spirals[3][4] = {{1,10,9,8},{2,11,12,7},{3,4,5,6}};
-   int col = sizeof(spirals[0]) / 4;
-   int row = (sizeof(spirals) / 4) / col;
-   
-
-   printSpiral(row,col,spirals, row*col);
+int main() {   
+   int spiral_a[3][4] = {{1,10,9,8},
+                         {2,11,12,7},
+                         {3,4,5,6}};
+   int spiral_b[3][3] = {{1, 8, 7},
+                         {2, 9, 6},
+                         {3, 4, 5}};
+   int col_a = sizeof (spiral_a[0]) / 4;
+   int row_a = (sizeof (spiral_a) / 4) / col_a;
+   int col_b = sizeof (spiral_b[0]) / 4;
+   int row_b = (sizeof (spiral_b) / 4 ) / col_b;
+ 
+   printArray (row_a, col_a, spiral_a);
+   printSpiral (row_a, col_a, spiral_a, row_a * col_a);
+   printArray (row_b, col_b, spiral_b);
+   printSpiral (row_b, col_b, spiral_b, row_b * col_b);
 
 }
